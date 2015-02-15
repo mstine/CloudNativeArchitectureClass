ba2ab8993bcae59ebc04e30f3760b0866a22b6a3
Matt Stine <mstine@gopivotal.com>
2015-02-15 11:40:01 -0800

    lab 10 complete

---
+++ day_01/session_03/lab_10/complete/cities/src/main/java/org/example/cities/config/CloudDataSourceConfig.java
@@ -0,0 +1,26 @@
+package org.example.cities.config;
+
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.PooledServiceConnectorConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Profile("cloud")
+@Configuration
+public class CloudDataSourceConfig extends AbstractCloudConfig {
+    @Bean
+    public DataSource dataSource() {
+        PooledServiceConnectorConfig.PoolConfig poolConfig =
+                new PooledServiceConnectorConfig.PoolConfig(5, 200);
+
+        DataSourceConfig.ConnectionConfig connectionConfig =
+                new DataSourceConfig.ConnectionConfig("characterEncoding=UTF-8");
+        DataSourceConfig serviceConfig = new DataSourceConfig(poolConfig, connectionConfig);
+
+        return connectionFactory().dataSource("cities-db", serviceConfig);
+    }
+}
---
+++ day_01/session_03/lab_10/complete/cities/src/main/java/org/example/cities/repositories/CityRepository.java
@@ -0,0 +1,24 @@
+package org.example.cities.repositories;
+
+import org.example.cities.domain.City;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+@RepositoryRestResource(collectionResourceRel = "cities", path = "cities")
+public interface CityRepository extends PagingAndSortingRepository<City, Long> {
+    @RestResource(path = "name", rel = "name")
+    Page<City> findByNameIgnoreCase(@Param("q") String name, Pageable pageable);
+
+    @RestResource(path = "nameContains", rel = "nameContains")
+    Page<City> findByNameContainsIgnoreCase(@Param("q") String name, Pageable pageable);
+
+    @RestResource(path = "state", rel = "state")
+    Page<City> findByStateCodeIgnoreCase(@Param("q") String stateCode, Pageable pageable);
+
+    @RestResource(path = "postalCode", rel = "postalCode")
+    Page<City> findByPostalCode(@Param("q") String postalCode, Pageable pageable);
+}
