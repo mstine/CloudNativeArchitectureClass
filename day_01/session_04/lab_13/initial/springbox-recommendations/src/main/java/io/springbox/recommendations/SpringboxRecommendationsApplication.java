b71bcbc7c6c4850eb81707f6093fca9719296e46
Matt Stine <mstine@gopivotal.com>
2015-02-16 18:08:40 -0800

    WIP checkpoint: finished local development porition of lab 13

---
+++ day_01/session_04/lab_13/initial/springbox-recommendations/src/main/java/io/springbox/recommendations/SpringboxRecommendationsApplication.java
@@ -0,0 +1,12 @@
+package io.springbox.recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboxRecommendationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboxRecommendationsApplication.class, args);
+    }
+}
