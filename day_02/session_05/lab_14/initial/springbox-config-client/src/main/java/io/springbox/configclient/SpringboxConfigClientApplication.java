c076729ec0667ea9a09d7f6eafd8a92975378c93
Matt Stine <mstine@gopivotal.com>
2015-02-18 10:51:22 -0800

    WIP checkpoint: lab 14 first two sections

---
+++ day_02/session_05/lab_14/initial/springbox-config-client/src/main/java/io/springbox/configclient/SpringboxConfigClientApplication.java
@@ -0,0 +1,12 @@
+package io.springbox.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboxConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboxConfigClientApplication.class, args);
+    }
+}
