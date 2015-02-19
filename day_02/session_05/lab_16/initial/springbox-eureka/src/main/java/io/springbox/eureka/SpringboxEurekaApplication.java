f2010df7ff74c0b7bd32224533d52f7f39095f1b
Matt Stine <mstine@gopivotal.com>
2015-02-18 18:56:44 -0800

    WIP checkpoint Lab 16: build and run eureka server done

---
+++ day_02/session_05/lab_16/initial/springbox-eureka/src/main/java/io/springbox/eureka/SpringboxEurekaApplication.java
@@ -0,0 +1,12 @@
+package io.springbox.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboxEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboxEurekaApplication.class, args);
+    }
+}
