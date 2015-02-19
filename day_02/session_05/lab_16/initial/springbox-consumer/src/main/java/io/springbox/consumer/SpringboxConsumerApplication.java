5211c9540fe506999abe6f5e4e3e90afe04544a0
Matt Stine <mstine@gopivotal.com>
2015-02-18 23:34:55 -0800

    WIP checkpoint Lab 16: consumer register and test done

---
+++ day_02/session_05/lab_16/initial/springbox-consumer/src/main/java/io/springbox/consumer/SpringboxConsumerApplication.java
@@ -0,0 +1,12 @@
+package io.springbox.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboxConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboxConsumerApplication.class, args);
+    }
+}
