3ae8eccf9be132eafffcb1e84dab09cffd31e377
Matt Stine <mstine@gopivotal.com>
2015-02-18 23:04:35 -0800

    add initial springbox-producer

---
+++ day_02/session_05/lab_16/initial/springbox-producer/src/main/java/io/springbox/producer/SpringboxProducerApplication.java
@@ -0,0 +1,12 @@
+package io.springbox.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboxProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboxProducerApplication.class, args);
+    }
+}
